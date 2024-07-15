e3b3ee44ffc4e30fe8d865228359bc654ab2e128
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-16 00:46:54 +0330

    Update views and funcs

--- Products/views.py
+++ Products/views.py
@@ -4,29 +4,19 @@ from .models import Product, ProductCategory
 from django.db.models import Avg, Max, Min
 
 
-def ProductListView(request):
-    # console = ProductCategory(title='پلی استیشن', url='playstation')
-    # console.save()
-    #
-    # ps_4 = Product(title='playstation4', price=14000000, is_active=True, rating=4, short_description='best console', category=console)
-    # ps_4.save()
-
-    product_list = Product.objects.all().order_by('title')
-    count = product_list.count()
-    avg_rating = product_list.aggregate(Avg('rating'), Min('price'), Max('rating'))
-    return render(request, 'Products/Products.html', {
+def products_list(request):
+    product_list = Product.objects.all().order_by('price')[:5]
+    return render(request, 'Products/products_list.html', {
         'product_list': product_list,
-        'products_count': count,
-        'Product_avg': avg_rating
     })
 
 
-def ProductDetailView(request, slug):
-    # try:
-    #     product = Product.objects.get(id=product_id)
-    # except:
-    #     raise Http404
-    product = get_object_or_404(Product, slug=slug)
-    return render(request, 'Products_detail/Products_detail.html', {
-        'products': product
-    })
+# def ProductDetailView(request, slug):
+#     # try:
+#     #     product = product.objects.get(id=product_id)
+#     # except:
+#     #     raise Http404
+#     product = get_object_or_404(Product, slug=slug)
+#     return render(request, 'Products_detail/Products_detail.html', {
+#         'products': product
+#     })
