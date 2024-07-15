c4727453836c875c5e2f94e69bbaa3445a9c2ec1
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-16 00:41:29 +0330

    Update setting file

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Products.apps.ProductsConfig'
+    'Products.apps.ProductsConfig',
+    'Home.apps.HomeConfig',
+    'django_render_partial'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
 TIME_ZONE = 'UTC'
 
