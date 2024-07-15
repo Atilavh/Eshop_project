d3a39719e8a328fa6897f6adb893631b5571b207
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-16 00:43:06 +0330

    Create & Add , url file & new urls

---
+++ Home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index_page, name='Home-page'),
+    path('contact-us', views.contact_page, name='Contact-page'),
+
+    # Django_render_partial_URLS {
+    # path('header-site', views.header_partial, name='header_partial'),
+    # path('footer-site', views.footer_partial, name='footer_partial')
+    # Django_render_partial_URLS }
+]
