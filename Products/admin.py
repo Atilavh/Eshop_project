040e6415c3cb4fcc653e7f70a545afc615267889
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-17 13:09:51 +0330

    Add new table in admin

--- Products/admin.py
+++ Products/admin.py
@@ -15,3 +15,4 @@ class ProductAdmin(admin.ModelAdmin):
 admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.ProductCategory)
 admin.site.register(models.ProductTags)
+admin.site.register(models.ProductBrand)
