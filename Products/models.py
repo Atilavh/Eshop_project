e7815d99345b5fe9aad68d29a6e125656678838f
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-17 13:14:33 +0330

    Update the Products module data base  & Add new table in db

--- Products/models.py
+++ Products/models.py
@@ -19,6 +19,18 @@ class ProductCategory(models.Model):
         verbose_name_plural = 'دسته بندی ها'
 
 
+class ProductBrand(models.Model):
+    title = models.CharField(max_length=100, verbose_name='عنوان برند', db_index=True)
+    is_active = models.BooleanField(verbose_name='فعال / غیرفعال')
+
+    class Meta:
+        verbose_name = 'برند'
+        verbose_name_plural = 'برندها'
+
+    def __str__(self):
+        return self.title
+
+
 class Product(models.Model):
     title = models.CharField(max_length=300, verbose_name='نام محصول')
     price = models.IntegerField(verbose_name='قیمت')
@@ -26,9 +38,11 @@ class Product(models.Model):
         ProductCategory,
         verbose_name='دسته بندی ها',
         related_name='product_categories')
+    Brand = models.ForeignKey(ProductBrand, on_delete=models.CASCADE, null=True, blank=True, verbose_name='برند')
     short_description = models.TextField(max_length=360, null=True, db_index=True, verbose_name='توضیحات کوتاه')
     description = models.TextField(db_index=True, verbose_name='توضیحات اصلی')
-    slug = models.SlugField(default="", null=False, db_index=True, blank=True, unique=True, max_length=200, verbose_name='ادرس دهی url')
+    slug = models.SlugField(default="", null=False, db_index=True, blank=True, unique=True, max_length=200,
+                            verbose_name='ادرس دهی url')
     is_active = models.BooleanField(default=False, verbose_name='فعال / غیرفعال')
     is_deleted = models.BooleanField(verbose_name='حذف شده / نشده', default=False)
 
@@ -48,7 +62,7 @@ class Product(models.Model):
 
 
 class ProductTags(models.Model):
-    caption = models.CharField(max_length=20, db_index=True,verbose_name='عنوان')
+    caption = models.CharField(max_length=20, db_index=True, verbose_name='عنوان')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_tags')
 
     def __str__(self):
