160eeaf6cc305c5433ad17b9b82eb18087309e11
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-22 10:18:20 +0330

    Update admin Contact-us

--- Contact_module/admin.py
+++ Contact_module/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from Contact_module.models import Contact_us
+
 
 # Register your models here.
+class Contact_usAdmin(admin.ModelAdmin):
+    list_display = ['title', 'email', 'full_name', 'message', 'is_read_by_admin']
+
+
+admin.site.register(Contact_us, Contact_usAdmin)
