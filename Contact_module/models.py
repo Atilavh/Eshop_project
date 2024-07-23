41ff15c18be03db589d23f2a0bab1f984cab9675
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-23 09:55:52 +0330

    update and edit field and module

--- Contact_module/models.py
+++ Contact_module/models.py
@@ -6,15 +6,20 @@ from django.db import models
 class Contact_us(models.Model):
     title = models.CharField(max_length=100, verbose_name="عنوان")
     email = models.EmailField(verbose_name="ایمیل")
-    full_name = models.CharField(max_length=100, verbose_name='نام کامل')
+    full_name = models.CharField(max_length=100, verbose_name='نام کامل', null=True, blank=True)
     message = models.TextField(max_length=200, verbose_name='متن تماس با ما')
-    is_read_by_admin = models.BooleanField(verbose_name='خوانده شده توسط ادمین')
+    is_read_by_admin = models.BooleanField(verbose_name='خوانده شده توسط ادمین', null=True)
     crated_date = models.DateTimeField(verbose_name='تاریخ ایجاد', auto_now_add=True)
-    response = models.TextField(verbose_name='متن پاسخ تماس با ما', null=True, blank=True)
+    response = models.TextField(verbose_name='متن پاسخ تماس با ما', blank=True)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = 'تماس با ما'
         verbose_name_plural = 'لیست تماس با ما'
 
-    def __str__(self):
-        return self.title
+
+class Profile_Picture(models.Model):
+    image = models.ImageField(upload_to='images', null=True, blank=True)
+
