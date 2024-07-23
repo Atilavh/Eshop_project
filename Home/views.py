fbd6a6cf5290c628dc8e11fc7b7a574b7089ab8c
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-23 09:59:08 +0330

    Add class home view for test

--- Home/views.py
+++ Home/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView
 
 
 # Create your views here.
 
-def index_page(request):
-    return render(request, 'shared/index.html')
+class HomeView(TemplateView):
+    template_name = 'shared/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = 'this is data'
+        context['message'] = 'this is message'
+        return context
+
+# def index_page(request):
+#     return render(request, 'shared/index.html')
 
 
 def header_partial(request):
