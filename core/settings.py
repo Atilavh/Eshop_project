7da203d7119d82c732296b93d2f401abee36d93f
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-23 09:57:35 +0330

    Add MEDIA_URL & MEDIA_ROOT

--- core/settings.py
+++ core/settings.py
@@ -108,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = 'fa-ir'
 
 TIME_ZONE = 'UTC'
@@ -122,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'img/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'staticfiles'
