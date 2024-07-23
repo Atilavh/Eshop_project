c772d0ff09512749bf52bcc4e32b7191e7756c2a
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-23 09:56:13 +0330

    Update url in contact module

--- Contact_module/urls.py
+++ Contact_module/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contact_us_page, name='contact-us'),
+    # path('', views.contact_us_page, name='contact-us'),
+    path('', views.ContactUsView.as_view(), name='contact-us'),
+    path('create-profile', views.upload_file, name='upload'),
 ]
