baa2cbf7f0beb211da57e29f9fbfb2d55f56aeff
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-23 09:58:46 +0330

    Update url in Home module

--- Home/urls.py
+++ Home/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_page, name='Home-page'),
-
+    # path('', views.index_page, name='Home-page'),
+    path('', views.HomeView.as_view(), name='Home-page'),
     # Django_render_partial_URLS {
     # path('header-site', views.header_partial, name='header_partial'),
     # path('footer-site', views.footer_partial, name='footer_partial')
