46d657acd59dc520ba6ab1d7c3fe0d7123265245
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-24 23:10:12 +0330

    Update Product's views and create session fun for project

--- Products/views.py
+++ Products/views.py
@@ -1,7 +1,6 @@
-from django.http import Http404
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product, ProductCategory
-from django.db.models import Avg, Max, Min
 from django.views.generic import ListView, DetailView, View
 from django.views.generic.base import TemplateView
 
@@ -10,16 +9,26 @@ class ProductListView(ListView):
     template_name = 'products/products_list.html'
     model = Product
     context_object_name = 'products'
+    ordering = ['price']
+    paginate_by = 1
 
     def get_queryset(self):
         base_query = super(ProductListView, self).get_queryset()
-        data = base_query.filter(is_active=True)
-        return data
+        # data = base_query.filter(is_active=True)
+        return base_query
 
 
 class ProductDetailView(DetailView):
     template_name = 'Products/product-details.html'
     model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_products = self.object
+        request = self.request
+        favorite_product_id = request.session.get('product_favorite')
+        context['is_favorite'] = favorite_product_id == str(loaded_products.id)
+        return context
 
     # def get_context_data(self, **kwargs):
     #     context = super(ProductDetailView, self).get_context_data()
@@ -40,8 +49,6 @@ class ProductDetailView(DetailView):
 #     return render(request, 'Products/products_list.html', {
 #         'product_list': product_list,
 #     })
-
-
 class AddFavoriteView(View):
     def post(self, request):
         product_id = request.POST['product_id']
