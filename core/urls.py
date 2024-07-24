7ff042858122e32d436b90936d2b347e40f2a02d
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-24 23:09:14 +0330

    Update core url & Add RegisterPage url

--- core/urls.py
+++ core/urls.py
@@ -20,9 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('Home.urls')),
+    path('RegisterPage/', include('accounts_module.urls')),
     path('contact-us/', include('Contact_module.urls')),
     path('', include('Products.urls')),
     path('admin/', admin.site.urls),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
