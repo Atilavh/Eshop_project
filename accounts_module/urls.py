c9cbce1b21610dd563d316e63e9e49a8bfa2c0fb
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-26 04:26:42 +0330

    Import new urls for forms

--- accounts_module/urls.py
+++ accounts_module/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('CreateAccount', views.Register, name='RegisterForm'),
+    path('LoginPage', views.login, name='LoginForm'),
+    path('activatecode/<str:email_active_code>', views.confirm_register, name='email_active_code'),
 ]
