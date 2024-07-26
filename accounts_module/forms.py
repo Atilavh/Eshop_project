c01159c9b797d8f9b394f48ebdcdc28973327774
AtilaVh <atilavalizadeh86@gmail.com>
2024-07-26 04:26:11 +0330

    Update forms.py and Add class login

--- accounts_module/forms.py
+++ accounts_module/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core import validators
+from django.core.exceptions import ValidationError
 
 
 class RegistrationForm(forms.Form):
@@ -6,6 +8,7 @@ class RegistrationForm(forms.Form):
         label='ایمیل',
         widget=forms.EmailInput
     )
+
     password = forms.CharField(
         label='کلمه عبور',
         widget=forms.PasswordInput
@@ -13,4 +16,44 @@ class RegistrationForm(forms.Form):
     confirm_password = forms.CharField(
         label='تایید کلمه عبور',
         widget=forms.PasswordInput
+    )
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+        if password == confirm_password:
+            return confirm_password
+
+        raise ValidationError('کلمه عبور و تکرار کلمه عبور باهم مغایرت دارد')
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        label='ایمیل',
+        widget=forms.EmailInput(),
+        validators=[
+            validators.MaxLengthValidator(100),
+        ]
+    )
+    password = forms.CharField(
+        label='پسوورد',
+        widget=forms.PasswordInput(),
+        validators=[
+            validators.MaxLengthValidator(100),
+        ]
+    )
+
+
+class Sing_up(forms.Form):
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(),
+    )
+    password = forms.CharField(
+        label='password',
+        widget=forms.PasswordInput(),
+    )
+    confirm_password = forms.CharField(
+        label='confirm_password',
+        widget=forms.PasswordInput(),
     )
