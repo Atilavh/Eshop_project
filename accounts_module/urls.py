db75a2f36425dafe3f8bc322a5d4e48a9025b118
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-05 23:38:40 +0330

    Import new urls in projects

--- accounts_module/urls.py
+++ accounts_module/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('CreateAccount', views.Register, name='RegisterForm'),
     path('LoginPage', views.login, name='LoginForm'),
+    path('ForgotPage', views.forgot_pass, name='ForgotPage'),
+    path('RestePassword/<active_code>', views.forgot_pass, name='ForgotPage'),
     path('activatecode/<str:email_active_code>', views.confirm_register, name='email_active_code'),
 ]
