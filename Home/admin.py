dd71258a9ed975fc89f296afbb56e604226c845c
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-10 18:19:37 +0330

    Update admin and import SiteSetting

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from Home.models import SiteSetting
+
+
 # Register your models here.
+
+class SiteSettingAdmin(admin.ModelAdmin):
+    list_display = ('site_name', 'site_url')
+
+
+admin.site.register(SiteSetting, SiteSettingAdmin)
