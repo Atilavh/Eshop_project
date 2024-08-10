2d38f51521caf96713d78826df661f647960e784
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-10 23:20:43 +0330

    Update article_module models.py file and Add class Article_List

--- article_module/models.py
+++ article_module/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from jalali_date import date2jalali, datetime2jalali
+
+from accounts_module.models import User
 
 
 # Create your models here.
@@ -16,3 +19,28 @@ class Article_Category(models.Model):
     class Meta:
         verbose_name = 'دسته بندی مقاله'
         verbose_name_plural = 'دسته بندی های مقاله'
+
+
+class Article_List(models.Model):
+    title = models.CharField(max_length=200, verbose_name='عنوان مقاله')
+    slug = models.SlugField(max_length=400, verbose_name='عنوان در url')
+    short_description = models.TextField(verbose_name='توضیحات کوتاه')
+    text = models.TextField(verbose_name='متن مقاله')
+    is_active = models.BooleanField(default=True, verbose_name='فعال / غیر فعال')
+    selected_categories = models.ManyToManyField(Article_Category, verbose_name='دسته بندی ها')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='نام نویسنده', null=True, blank=True,
+                               editable=False)
+    create_date = models.DateTimeField(auto_now_add=True, editable=False, verbose_name='تاریخ ثبت')
+
+    def __str__(self):
+        return self.title
+
+    def get_jalali_create_date(self):
+        return date2jalali(self.create_date)
+
+    def get_jalali_create_time(self):
+        return self.create_date.strftime('%H:%M')
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
