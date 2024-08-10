a4dfad6840441569c1b0d6cc54f54c76ffb6e7b7
AtilaVh <atilavalizadeh86@gmail.com>
2024-08-10 23:19:30 +0330

    Update article_module admin.py Add new class

--- article_module/admin.py
+++ article_module/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-
-from article_module.models import Article_Category
+from django.http import HttpResponse
+from article_module.models import Article_Category, Article_List
 
 
 # Register your models here.
@@ -10,4 +10,15 @@ class Article_Admin(admin.ModelAdmin):
     list_editable = ['url_title', 'is_active', 'parent']
 
 
+class Article_List_Admin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'is_active', 'author']
+    list_editable = ['is_active']
+
+    def save_model(self, request: HttpResponse, obj: Article_List, form, change):
+        if not change:
+            obj.author = request.user
+        return super().save_model(request, obj, form, change)
+
+
 admin.site.register(Article_Category, Article_Admin)
+admin.site.register(Article_List, Article_List_Admin)
